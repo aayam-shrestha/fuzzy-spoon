d231b96d19dae157347c49216e3660ec0857af2d
Anwesha-P <anwesha.pradhananga@gmail.com>
2022-03-26 13:26:57 -0400

    working

--- main.py
+++ main.py
@@ -1,53 +1,14 @@
-import os, sys, math, pygame, pygame.mixer
-from pygame.locals import *
+from game import Game
+import pygame
+from platforms import Platform
 
-black = (0,0,0)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-white = (255,255,255)
-run_me = True
+width, height = 1375, 1000
+win = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height))
+g = Game(win,screen, width, height)
+g.main()
 
-screen_size = screen_width, screen_height = 1000, 500
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption('platformer')
 
-window = pygame.display.set_mode((1000,500))
-clock = pygame.time.Clock()
-fps_limit = 60
+       
 
-picture = pygame.image.load('download.png')
-bg = pygame.image.load('tree.png')
 
-##bg = pygame.transform.scale(bg, screen_size)
-
-posx = 300
-posy = 200
-
-while run_me:
-    clock.tick(fps_limit) 
-
-    window.blit(bg, (0,0))
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            run_me = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                posx = posx - 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                posx = posx + 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                posy = posy - 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                posy = posy + 10
-                
-    screen.blit(picture, (posx, posy))
-    pygame.display.update()
-    pygame.display.flip()
-
-    
-
-pygame.quit()
